f3f42debed8cb96ff3d33cddf9d2c79e2ab7fb61
n1sh3br0d <roma461662@yandex.ru>
2018-09-28 18:45:06 +0300

    addErrorStyling

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import './main.css';
+
 class Error extends Component {
   render() {
     return(
       <React.Fragment>
         <div className='Error'>
-          <span>{this.props.message}</span>
+          <span>{this.props.message}&#10004;</span>
         </div>
       </React.Fragment>
     )
